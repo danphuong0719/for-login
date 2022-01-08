d30adad42d84d35339e72cbbf54bd9fd0c0b0d82
danphuong0719 <nguyendangphuong0719@gmail.com>
2022-01-08 18:49:53 +0700

    first commit

---
+++ ConTro.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// bien -> [dia chi (Địa chỉ chứa giá trị) -> giá trị]
+	int a = 5;
+
+	int& c = a;
+
+	int *b = &a;
+
+	// b = &a = @abasadadasasd
+	// 	*b = *(&a) => a = 5 = > *b = 5
+
+	cout <<  *b  << ", " << &a; // 
+
+
+}
+
